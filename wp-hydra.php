4780f5f003e0eb7e1cca2300b264a60b66e932cb
Marin Atanasov <tyxla@abv.bg>
2016-02-13 12:34:49 +0200

    setup_domain() host sanitization

--- wp-hydra.php
+++ wp-hydra.php
@@ -72,7 +72,7 @@ class WP_Hydra {
 
 		// get original and current domain
 		$original_domain = $original_domain_parts['host'];
-		$current_domain = sanitize_text_field( $_SERVER['HTTP_HOST'] ); // input var ok.
+		$current_domain = sanitize_text_field( $_SERVER['HTTP_HOST'] ); // WPCS: sanitization okay
 
 		// if original and current domain match, skip
 		if ( $original_domain == $current_domain ) {
