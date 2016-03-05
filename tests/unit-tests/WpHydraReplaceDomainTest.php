47a82e0325137e83cf7181e236b5555a6d5af793
Marin Atanasov <tyxla@abv.bg>
2016-03-05 12:36:20 +0200

    done tests for WP_Hydra::replace_domain()

--- tests/unit-tests/WpHydraReplaceDomainTest.php
+++ tests/unit-tests/WpHydraReplaceDomainTest.php
@@ -37,6 +37,90 @@ class WpHydraReplaceDomainTest extends WP_UnitTestCase {
 		$this->assertSame( $expected, $actual );
 	}
 
+	/**
+	 * @covers WP_Hydra::replace_domain
+	 */
+	public function testWithHttpsUrlAndHttpsOff() {
+		$this->wp_hydra->expects($this->any())
+			->method('is_ssl')
+			->will($this->returnValue(false));
+
+		$url = 'https://example.com/';
+		$expected = 'http://example.com/';
+		$actual = $this->wp_hydra->replace_domain_exposed( $url, 'example.com', 'example.com' );
+		$this->assertSame( $expected, $actual );
+	}
+
+	/**
+	 * @covers WP_Hydra::replace_domain
+	 */
+	public function testWithHttpsUrlAndHttpsOn() {
+		$this->wp_hydra->expects($this->any())
+			->method('is_ssl')
+			->will($this->returnValue(true));
+
+		$url = 'https://example.com/';
+		$actual = $this->wp_hydra->replace_domain_exposed( $url, 'example.com', 'example.com' );
+		$this->assertSame( $url, $actual );
+	}
+
+	/**
+	 * @covers WP_Hydra::replace_domain
+	 */
+	public function testWithHttpUrlAndHttpsOffAndDifferentDomain() {
+		$this->wp_hydra->expects($this->any())
+			->method('is_ssl')
+			->will($this->returnValue(false));
+
+		$url = 'http://example.com/';
+		$expected = 'http://foobar.com/';
+		$result = $this->wp_hydra->replace_domain_exposed( $url, 'example.com', 'foobar.com' );
+		$this->assertSame( $expected, $result );
+	}
+
+	/**
+	 * @covers WP_Hydra::replace_domain
+	 */
+	public function testWithHttpUrlAndHttpsOnAndDifferentDomain() {
+		$this->wp_hydra->expects($this->any())
+			->method('is_ssl')
+			->will($this->returnValue(true));
+
+		$url = 'http://example.com/';
+		$expected = 'https://foobar.com/';
+		$actual = $this->wp_hydra->replace_domain_exposed( $url, 'example.com', 'foobar.com' );
+		$this->assertSame( $expected, $actual );
+	}
+
+	/**
+	 * @covers WP_Hydra::replace_domain
+	 */
+	public function testWithHttpsUrlAndHttpsOffAndDifferentDomain() {
+		$this->wp_hydra->expects($this->any())
+			->method('is_ssl')
+			->will($this->returnValue(false));
+
+		$url = 'https://example.com/';
+		$expected = 'http://foobar.com/';
+		$actual = $this->wp_hydra->replace_domain_exposed( $url, 'example.com', 'foobar.com' );
+		$this->assertSame( $expected, $actual );
+	}
+
+	/**
+	 * @covers WP_Hydra::replace_domain
+	 */
+	public function testWithHttpsUrlAndHttpsOnAndDifferentDomain() {
+		$this->wp_hydra->expects($this->any())
+			->method('is_ssl')
+			->will($this->returnValue(true));
+
+		$url = 'https://example.com/';
+		$expected = 'https://foobar.com/';
+		$actual = $this->wp_hydra->replace_domain_exposed( $url, 'example.com', 'foobar.com' );
+		$this->assertSame( $expected, $actual );
+	}
+
+
 }
 
 class WP_Hydra_Exposed_RD extends WP_Hydra {
